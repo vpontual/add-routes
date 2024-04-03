4b8f831c0f909a5382d8fba9ca6dca4b1b31c0e4
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-03 10:46:37 -0400

    added 404 page and route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
+const tipsRouter = require("./tips");
+const feedbackRouter = require("./feedback");
 // TODO: import your diagnostics route
+const diagnostics = require("express").Router();
 
-router.use('/tips', tipsRouter);
-router.use('/feedback', feedbackRouter);
+router.use("/tips", tipsRouter);
+router.use("/feedback", feedbackRouter);
 // TODO: Initialize diagnostics route
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+const api = require("./routes/index.js");
 
 const PORT = process.env.port || 3001;
 
@@ -13,19 +13,24 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use("/api", api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get("/feedback", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
 );
+
+app.get("*", (req, res) => {
+  res.status(404),
+    sendFile(path.join(__dirname, "public", "pages", "404.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
